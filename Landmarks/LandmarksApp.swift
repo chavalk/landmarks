4b23784c3d9302460b304643d0ee9c4abb1b4fcd
chavalk <chavalk@hotmail.com>
2023-04-29 09:31:11 -0500

    First commit.

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jose Garcia on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
