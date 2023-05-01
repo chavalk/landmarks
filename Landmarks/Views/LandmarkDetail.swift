887fd34fe9d79c151fafda21a9a8c939cdddece1
chavalk <chavalk@hotmail.com>
2023-04-30 19:00:15 -0500

    Change vstack to scrollview and remove spacer.

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -11,7 +11,7 @@ struct LandmarkDetail: View {
     var landmark: Landmark
     
     var body: some View {
-        VStack {
+        ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
                 .ignoresSafeArea(edges: .top)
                 .frame(height: 300)
@@ -39,8 +39,6 @@ struct LandmarkDetail: View {
                 Text(landmark.description)
             }
             .padding()
-            
-            Spacer()
         }
     }
 }
