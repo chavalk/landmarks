550dcaed704a690b6b048eb55baaaada772177cf
chavalk <chavalk@hotmail.com>
2023-05-04 19:34:56 -0500

    Add category home file.

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Jose Garcia on 5/4/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
