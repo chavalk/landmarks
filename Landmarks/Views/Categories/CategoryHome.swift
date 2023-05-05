b1da4c55cad614e76e56f85c0e5b8a372aae40a9
chavalk <chavalk@hotmail.com>
2023-05-04 20:32:26 -0500

    Add model data as environment object to category home.

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -8,9 +8,11 @@
 import SwiftUI
 
 struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
     var body: some View {
         NavigationStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("Landmarks Content")
                 .navigationTitle("Featured")
         }
     }
@@ -19,5 +21,6 @@ struct CategoryHome: View {
 struct CategoryHome_Previews: PreviewProvider {
     static var previews: some View {
         CategoryHome()
+            .environmentObject(ModelData())
     }
 }
