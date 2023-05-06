39db00fb887c5f1fbf0fc8b136c65adc81e1542a
chavalk <chavalk@hotmail.com>
2023-05-06 16:46:46 -0500

    Add profile host.

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Jose Garcia on 5/6/23.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        Text("Profile for: \(draftProfile.username)")
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
