ed313a222b370dc7a2f1d3d7d6c2f0450f5e3376
chavalk <chavalk@hotmail.com>
2023-05-07 14:24:07 -0500

    Initialize array of controllers using pages array of views.

--- Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -24,14 +24,17 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
     }
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
-        pageViewController.setViewControllers([UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+        pageViewController.setViewControllers(
+            [context.coordinator.controllers[0]], direction: .forward, animated: true)
     }
     
     class Coordinator: NSObject {
         var parent: PageViewController
+        var controllers = [UIViewController]()
         
         init(_ pageViewController: PageViewController) {
             parent = pageViewController
+            controllers = parent.pages.map { UIHostingController(rootView: $0) }
         }
     }
 }
