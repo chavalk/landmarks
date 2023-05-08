47a4335a31fb044f3da8c3bb22e1ae4c28d9117f
chavalk <chavalk@hotmail.com>
2023-05-08 11:59:39 -0500

    Add watch os companion app.

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Jose Garcia on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchLandmarks/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks
+//
+//  Created by Jose Garcia on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
